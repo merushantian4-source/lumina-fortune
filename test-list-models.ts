3bbedb4a758eb364448c14c67257789f599ffeee
merushantian4-ソース <merushantian@gmail.com>
2026-02-22 20:20:22 +0900

    Remove local test script from build

--- test-list-models.ts
+++
@@ -1,9 +0,0 @@
-import { GoogleGenerativeAI } from "@google/generative-ai"
-
-async function listModels() {
-  const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!)
-  const res = await genAI.listModels()
-  console.log(res)
-}
-
-listModels()
